3c783c7f25fcdf3d640deefc2405d8358f649899
Felix Schmid <77964825+Flixl09@users.noreply.github.com>
2026-02-20 08:42:59 +0100

    Revert "Dev/capacitor setup"

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.nimmit.app',
-  appName: 'nimmit',
-  webDir: 'dist/nimmit/browser',
-};
-
-export default config;
