b92256f371a2ac377e8410a577d8a3a0b1b4c5e9
Felix Schmid <felix20.09@hotmail.com>
2026-03-13 10:52:37 +0100

    fixed testing site

--- src/app/authentication/account/account.spec.ts
+++ src/app/authentication/account/account.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 
 import { Account } from './account';
 
@@ -8,7 +9,8 @@ describe('Account', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Account]
+      imports: [Account],
+      providers: [provideRouter([])]
     })
     .compileComponents();
 
