de4447bd631795bcd0f32f52c719fbf2ed9cb304
Felix Schmid <felix20.09@hotmail.com>
2026-03-20 10:51:03 +0100

    powersync install and config url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: false,
   supabaseUrl: 'https://oioegtvwcxizjbbvktdv.supabase.co',
   supabaseKey: 'sb_publishable_mJwA2O66h4ttiWmDPHdNSw_Enoo38mX',
+  powersyncUrl: 'https://69bc0eacf2dc1cca3f505685.powersync.journeyapps.com'
 }
